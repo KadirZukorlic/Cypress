94439b762841e09f5d6eae3a5ff80d37e59501b7
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-10-23 18:38:00 +0200

    run prettier

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -3,10 +3,9 @@
 describe("Suit", () => {
 
   it("finding elements", () => {
-
-    cy.visit('/') // slash / because we defined baseUrl in config file
-    cy.contains('Forms').click()
-    cy.contains('Form Layouts').click()
+    cy.visit("/"); // slash / because we defined baseUrl in config file
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
 
     // find elements
     // by Tag Name
@@ -16,25 +15,25 @@ describe("Suit", () => {
     cy.get("#inputEmail1");
 
     // by class name
-    cy.get('.input-full-width')
+    cy.get(".input-full-width");
 
     // by attribute name
-    cy.get('[placeholder]')
+    cy.get("[placeholder]");
 
     // by attribute name and value
-    cy.get('[placeholder="Email"]')
+    cy.get('[placeholder="Email"]');
 
     // by class - value have to provide entire value from class
-    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
 
     // by Tag name and Attribute with value
-    cy.get('input[placeholder="Email"]')
+    cy.get('input[placeholder="Email"]');
 
     // by two different attributes
-    cy.get('[placeholder="Email"][fullwidth]')
+    cy.get('[placeholder="Email"][fullwidth]');
 
     // by Tag name, attribute with value, id and class name
-    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
 
     // the most recommended way by cypress
     cy.get('[data-cy="imputEmail1"]')
