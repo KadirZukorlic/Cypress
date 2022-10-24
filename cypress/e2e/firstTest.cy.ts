75a8bfe9ac06ef952507c39570a6dbc948e0d440
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-10-24 04:43:29 +0200

    then and wrap methods

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -59,4 +59,71 @@ describe("Suit", () => {
 
     cy.contains("nb-card", "Horizontal form").find('[type="email"]');
   });
+
+  it("then and wrap methods", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    // cy.contains("nb-card", "Using the Grid")
+    //   .find('[for="inputEmail1"]')
+    //   .should("contain", "Email");
+
+    // cy.contains("nb-card", "Using the Grid")
+    //   .find('[for="inputPassword2"]')
+    //   .should("contain", "Password");
+
+    // cy.contains("nb-card", "Basic form")
+    //   .find('[for="exampleInputEmail1"')
+    //   .should("contain", "Email address");
+
+    // cy.contains("nb-card", "Basic Form")
+    //   .find('[for="exampleInputPassword1"]')
+    //   .should("contain", "Password");
+
+    // Selenium
+
+    // const firstForm = cy.contains("nb-card", "Using the Grid");
+    // const secondForm = cy.contains("nb-card", "Basic form");
+
+    // firstForm
+    //   .find('[for="exampleInputEmail1"]')
+    //   .should("contain", "Email address");
+    // secondForm
+    //   .find('[for="exampleInputPassword1"]')
+    //   .should("contain", "Password");
+
+    // doesn't work in cypress because cypress is synchronus
+
+    // Cypress style
+
+    cy.contains("nb-card", "Using the Grid").then((firstForm) => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+      const passwordLabelFirst = firstForm
+        .find('[for="inputPassword2"]')
+        .text();
+      expect(emailLabelFirst).to.equal("Email");
+      expect(passwordLabelFirst).to.equal("Password");
+
+      cy.contains("nb-card", "Basic form").then((secondForm) => {
+        const emailLabelSecond = secondForm
+          .find('[for="exampleInputEmail1"]')
+          .text();
+
+        expect(emailLabelFirst).not.to.equal(emailLabelSecond);
+      });
+
+      cy.contains("nb-card", "Basic form").then((secondForm) => {
+        const passwordLabelSecond = secondForm
+          .find('[for="exampleInputPassword1"]')
+          .text();
+
+        expect(passwordLabelSecond).to.equal(passwordLabelFirst);
+
+        cy.wrap(secondForm)
+          .find('[for="exampleInputPassword1"]')
+          .should("contain", "Password");
+      });
+    });
+  });
 });
