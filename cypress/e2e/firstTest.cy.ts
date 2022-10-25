d790259d65ed300dbf0f30ffd03a860a55fb80db
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-10-25 05:12:37 +0200

    Invoke function

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -126,4 +126,37 @@ describe("Suit", () => {
       });
     });
   });
+
+  it("Invoke Command", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    // 1
+    cy.get('[for="exampleInputEmail1"]').should("contain", "Email address");
+
+    // 2
+    cy.get('[for="exampleInputEmail1"]').then((inputLabel) => {
+      const inputLabelText = inputLabel.text();
+
+      expect(inputLabelText).to.equal("Email address");
+    });
+
+    // 3
+    cy.get('[for="exampleInputEmail1"]')
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal("Email address");
+      });
+
+    cy.contains("nb-card", "Basic form")
+      .find("nb-checkbox")
+      .click()
+      .find(".custom-checkbox")
+      .invoke("attr", "class")
+      // .should("contain", "checked");
+      .then((classValue) => {
+        expect(classValue).to.contain("checked");
+      });
+  });
 });
