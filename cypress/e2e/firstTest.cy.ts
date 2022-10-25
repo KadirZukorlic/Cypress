ccd7c20b42f3c1f190f9e09fd05ec01051e368c1
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-10-25 23:19:01 +0200

    Test radio butons and checkboxes

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -160,7 +160,7 @@ describe("Suit", () => {
       });
   });
 
-  it.only("Assert Property", () => {
+  it("Assert Property", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
@@ -174,5 +174,37 @@ describe("Suit", () => {
           .invoke("prop", "value")
           .should("contain", "Oct 17, 2022");
       });
+  });
+
+  it("Radio button", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type="radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({ force: true })
+          .should("be.checked");
+
+        // eq(index)
+        cy.wrap(radioButtons).eq(1).check({ force: true });
+
+        cy.wrap(radioButtons).eq(0).should("not.be.checked");
+
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
+
+  it.only("Check boxes", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    // cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+    cy.get('[type="checkbox"]').eq(0).check({ force: true });
   });
 });
