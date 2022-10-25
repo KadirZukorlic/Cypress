594632e4bad393b1200a7428f387e8fcf6d2e629
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-10-25 05:22:41 +0200

    Assert property of an object

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -159,4 +159,20 @@ describe("Suit", () => {
         expect(classValue).to.contain("checked");
       });
   });
+
+  it.only("Assert Property", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Datepicker").click();
+
+    cy.contains("nb-card", "Common Datepicker")
+      .find("input")
+      .then((input) => {
+        cy.wrap(input).click();
+        cy.get("nb-calendar-day-picker").contains("17").click();
+        cy.wrap(input)
+          .invoke("prop", "value")
+          .should("contain", "Oct 17, 2022");
+      });
+  });
 });
