43978d20abcca8a1a800aad23252b5f2461c8791
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-10-28 00:45:08 +0200

    Test Lists and Dropdowns

--- cypress/e2e/tests.cy.ts
+++ cypress/e2e/tests.cy.ts
@@ -198,7 +198,7 @@ describe("Suit", () => {
       });
   });
 
-  it.only("Check boxes", () => {
+  it("Check boxes", () => {
     cy.visit("/");
     cy.contains("Modal & Overlays").click();
     cy.contains("Toastr").click();
@@ -207,4 +207,35 @@ describe("Suit", () => {
     cy.get('[type="checkbox"]').eq(0).click({ force: true });
     cy.get('[type="checkbox"]').eq(0).check({ force: true });
   });
+
+  it.only('List and Dropdowns', () => {
+    cy.visit("/")
+    // 1
+    cy.get('nav nb-select').click()
+    cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+    
+    // 2
+    cy.get('nav nb-select').then(dropdown => {
+      cy.wrap(dropdown).click()
+      cy.get('.options-list nb-option').each((listItem, index) => {
+       const itemText = listItem.text().trim()
+
+       const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)",
+       }
+
+       cy.wrap(listItem).click()
+       cy.wrap(dropdown).should('contain', itemText)
+       cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+       if (index < 3) {
+        cy.wrap(dropdown).click()
+       }
+      })
+    })
+  })
 });
