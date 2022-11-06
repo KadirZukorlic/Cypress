624b22856fcbdd3a4aefe8b736dcee7667f6558e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-11-06 03:12:56 +0100

    Add submitBasicFormWithEmailAndPassword

--- cypress/support/page-objects/formLayoutPage.ts
+++ cypress/support/page-objects/formLayoutPage.ts
@@ -9,6 +9,16 @@ export class FormLayoutPage {
         cy.wrap(form).submit();
       });
   }
+  submitBasicFormWithEmailAndPassword(email: string, password: string) {
+    cy.contains("nb-card", "Basic form")
+      .find("form")
+      .then((form) => {
+        cy.wrap(form).find('[placeholder="Email"]').type(email);
+        cy.wrap(form).find('[placeholder="Password"]').type(password);
+        cy.wrap(form).find('[type="checkbox"]').check({ force: true });
+        cy.wrap(form).submit();
+      });
+  }
 }
 
 export const onFormLayoutPage = new FormLayoutPage();
